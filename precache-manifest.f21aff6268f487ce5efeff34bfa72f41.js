f48de5ae514c9f401b60edec07d43e57a0c438af
Carlos Garrido <carl.garrido.aedo@gmail.com>
2020-01-22 17:17:01 -0300

    Updates

---
+++ precache-manifest.f21aff6268f487ce5efeff34bfa72f41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d213b02448a48cfd6f64ba8184a20712",
+    "url": "/reconocimiento_facial/index.html"
+  },
+  {
+    "revision": "e7c9e12ffea6f2c1e0a1",
+    "url": "/reconocimiento_facial/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0d3ee78793eda6a7ab89",
+    "url": "/reconocimiento_facial/static/css/main.0c38f7cc.chunk.css"
+  },
+  {
+    "revision": "e7c9e12ffea6f2c1e0a1",
+    "url": "/reconocimiento_facial/static/js/2.1866cde9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reconocimiento_facial/static/js/2.1866cde9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d3ee78793eda6a7ab89",
+    "url": "/reconocimiento_facial/static/js/main.1912dd34.chunk.js"
+  },
+  {
+    "revision": "a98e848154c9641a890b",
+    "url": "/reconocimiento_facial/static/js/runtime-main.78a605f1.js"
+  },
+  {
+    "revision": "a66bb09687acf7164f65c033c330bc4f",
+    "url": "/reconocimiento_facial/static/media/penguin.a66bb096.png"
+  }
+]);

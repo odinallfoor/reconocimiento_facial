85b5c2ba19c9531af6f59301a127e5e3b2e3fb4c
Carlos Garrido <carl.garrido.aedo@gmail.com>
2020-01-28 16:13:58 -0300

    Se devuelve titulo del boton

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -24,7 +24,7 @@ const Signin = ({onRouteChange}) => {
                             onClick={() => onRouteChange('home')}
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                             type="submit"
-                            value="Ingresar"
+                            value="Sign In"
                         />
                     </div>
                     <div className="lh-copy mt3">
